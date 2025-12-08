3809b4c1150aa8bc8e600cf35ce2d20daf661144
girochat <117285316+girochat@users.noreply.github.com>
2025-12-08 11:01:26 +0100

    Bugfix: removed ancient package reference

--- tests/test_valid_args_schemas.py
+++ tests/test_valid_args_schemas.py
@@ -29,7 +29,7 @@ def test_task_functions_have_valid_signatures():
                 continue
             function_name = Path(executable).with_suffix("").name
             task_function = _extract_function(
-                    executable, function_name, package_name="skinnervation3d_fractal_tasks"
+                    executable, function_name, package_name="mesospim_fractal_tasks"
                     )
             _validate_function_signature(task_function)
 
@@ -46,7 +46,7 @@ def test_args_schemas_are_up_to_date():
             print(f"Now handling {executable}")
             old_schema = task[f"args_schema_{kind}"]
             new_schema = create_schema_for_single_task(
-                    executable, package="skinnervation3d_fractal_tasks"
+                    executable, package="mesospim_fractal_tasks"
                     )
             # The following step is required because some arguments may have a
             # default which has a non-JSON type (e.g. a tuple), which we need

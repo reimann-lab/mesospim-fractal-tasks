1b5c7383b05fac89b2287428357cfb2659b9a2e8
girochat <117285316+girochat@users.noreply.github.com>
2026-01-19 15:56:06 +0100

    Dask parallel version of crop ROI task (#27)

---
+++ src/mesospim_fractal_tasks/tasks/crop_regions_of_interest_dask.py
@@ -0,0 +1,443 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
+import numcodecs
+numcodecs.blosc.set_nthreads(1)
+
+import logging
+from pathlib import Path
+import zarr
+from dask.distributed import Client
+import dask.array as da
+import pandas as pd
+import numpy as np
+import anndata as ad
+from typing import Dict, Any
+from pydantic import validate_call
+
+from fractal_tasks_core.ngff import load_NgffImageMeta
+from fractal_tasks_core.roi import get_single_image_ROI, prepare_FOV_ROI_table
+from fractal_tasks_core.tables import write_table
+
+from mesospim_fractal_tasks.utils.zarr_utils import (_determine_optimal_contrast,
+                                                     _update_omero_channels,
+                                                     build_pyramid)
+from mesospim_fractal_tasks.utils.parallelisation import (_set_dask_cluster,
+                                                          build_pyramid_per_channel)
+
+logger = logging.getLogger(__name__)
+
+def adapt_coordinates(
+    start: float, 
+    end: float, 
+    dim: str, 
+    scale: float, 
+    table: pd.DataFrame
+) -> None:
+    """
+    Adapt the entries of the FOV ROI table for the given coordinates per dimension.
+
+    Parameters:
+        start (float): New start pixel coordinate.
+        end (float): New end pixel coordinate.
+        dim (str): Dimension to adapt.
+        scale (float): Pixel scale in um.
+        table (pd.DataFrame): Original FOV ROI Table to be adapted.
+    """
+    new_start_um = start * scale
+    new_end_um = end * scale
+    table[f"pixel_size_{dim}"] = scale
+    table[f"{dim}_micrometer"] = table[f"{dim}_micrometer"] - new_start_um
+    table[f"{dim}_micrometer"] = table[f"{dim}_micrometer"].clip(
+        lower=0, upper=(new_end_um-new_start_um))
+    if dim != "z":
+        table[f"{dim}_micrometer_original"] = (table[f"{dim}_micrometer_original"] - 
+                                               new_start_um)
+        table[f"{dim}_micrometer_original"] = table[f"{dim}_micrometer_original"].clip(
+            lower=0, upper=(new_end_um-new_start_um))
+    dim_max = table[f"{dim}_micrometer"].max()
+    if dim_max == (new_end_um-new_start_um):
+        table.drop(table[table[f"{dim}_micrometer"] == dim_max].index, inplace=True)
+    
+    dim_micrometers = sorted(table[f"{dim}_micrometer"].unique())
+    dim_micrometers.append(new_end_um-new_start_um)
+    dim_micrometers = np.array(dim_micrometers)
+    for r, row in table.iterrows():
+        i = np.argwhere(dim_micrometers == row[f"{dim}_micrometer"])[0][0]
+        table.loc[r,f"len_{dim}_micrometer"] = (dim_micrometers[i+1] - 
+                                                row[f"{dim}_micrometer"])
+        table.loc[r,f"{dim}_pixel"] = int(round(table.loc[r, f"len_{dim}_micrometer"] 
+                                                / scale))
+    
+def adapt_roi_table(
+    zarr_path: Path, 
+    roi_path: Path, 
+    coords: dict[str, int], 
+    scale: tuple[float, float, float]
+) -> pd.DataFrame:
+    """
+    Adapt the FOV ROI table to the new crop coordinates.
+
+    Parameters:
+        zarr_path (Path): Path to the OME-Zarr image.
+        roi_path (Path): Path to the ROI table.
+        coords (dict): Coordinates to adapt.
+        scale (tuple[float, float, float]): Pixel scale in um.
+
+    Returns:
+        pandas.DataFrame: Adaptated FOV ROI table.
+    """
+    logger.info(f"Adapting FOV ROI table to new crop coordinates for {roi_path}")
+
+    # Load original table
+    source_table = ad.read_zarr(zarr_path/ "tables" / "FOV_ROI_table").to_df()
+
+    # Update z, y, x coordinates
+    x_start, x_end = coords["x_start"], coords["x_end"]
+    y_start, y_end = coords["y_start"], coords["y_end"]
+    z_start, z_end = coords["z_start"], coords["z_end"]
+    adapt_coordinates(x_start, x_end, "x", scale[2], source_table)
+    adapt_coordinates(y_start, y_end, "y", scale[1], source_table)
+    adapt_coordinates(z_start, z_end, "z", scale[0], source_table)
+
+    # Update index
+    source_table.index = [i for i in range(len(source_table))]
+    return source_table
+
+def check_binary_compatibility(
+    slice_start: float, 
+    slice_end: float,
+    max_end: int,
+    scale: float,
+    power: int = 6
+) -> tuple[int, int]:
+    """
+    Check if the crop slices can be divided by a power of 2.
+    
+    Parameters:
+        slice_start (float): Beginning of slice in microns.
+        slice_end (float): End of slice in microns.
+        max_end (int): Maximum possible end of slice in px.
+        scale (float): Scale of the image.
+        power (int): Power of 2 to check (should match pyramid resolution).
+    
+    Returns:
+        tuple[int, int]: New slice start and end in pixels.
+    """
+    modulo = abs(round(slice_end / scale) - round(slice_start / scale)) % 2**power
+    if modulo > 0:
+        add_start = (2**power - modulo) // 2
+        temp_start = round(slice_start / scale)
+        if add_start > temp_start:
+            add_start = temp_start
+        new_slice_start = temp_start - add_start
+        new_slice_end = round(slice_end / scale) + (2**power - modulo - add_start)
+    else:
+        new_slice_start = round(slice_start / scale)
+        new_slice_end = round(slice_end / scale)
+    if new_slice_end > max_end:
+        diff = new_slice_end - slice_end
+        new_slice_end = max_end
+        add_start = 2**power - (diff % 2**power)
+        new_slice_start = new_slice_start - add_start
+        if new_slice_start < 0:
+            new_slice_start = 0
+            logger.warning(f"Crop is outside of array boundary. Keeping original size.")
+        
+    return int(new_slice_start), int(new_slice_end)
+
+def save_roi_parallel(
+    zarr_path: Path,
+    full_res_arr: da.Array,
+    id: str,
+    coords: dict[str, int],
+    scale: tuple[float, ...],
+    num_levels: int,
+    coarsening_xy: int,
+    crop_or_roi: str,
+) -> None:
+    """
+    Save a ROI from a multi-channel OME-Zarr image to a new OME-Zarr image.
+
+    Parameters:
+        zarr_path (Path): Path to the OME-Zarr image to be processed.
+        full_res_arr (da.Array): Array of the full resolution image.
+        id (str): ID of the ROI to save.
+        coords (dict[str, int]): Coordinates of the ROI to save.
+        scale (tuple[float, float, float]): Pixel scale in um.
+        num_levels (int): Number of pyramid levels.
+        coarsening_xy (int): Coarsening factor in XY.
+    """
+    full_shape = full_res_arr.shape
+    z_start, z_end = check_binary_compatibility(max(coords['z_start_um'], 0),
+                                                    coords['z_end_um'] + scale[0],
+                                                    full_shape[1],
+                                                    scale[0], 
+                                                    power=0)
+    y_start, y_end = check_binary_compatibility(max(coords['y_start_um'], 0),
+                                                    coords['y_end_um'] + scale[1], 
+                                                    full_shape[2],
+                                                    scale[1],
+                                                    power=num_levels)
+    x_start, x_end = check_binary_compatibility(max(coords['x_start_um'], 0),
+                                                    coords['x_end_um'] + scale[2],
+                                                    full_shape[3], 
+                                                    scale[2],
+                                                    power=num_levels)
+
+    # Crop region
+    logger.info(f"Cropping ROI region from full resolution image at "
+                    f"{z_start}:{z_end}, {y_start}:{y_end}, {x_start}:{x_end}.")
+    crop = full_res_arr[:,
+                        z_start:z_end,
+                        y_start:y_end,
+                        x_start:x_end]
+    
+    logger.info(f"Saving cropped region as {id}.")
+    root_path = zarr_path.parent
+    roi_path = Path(root_path, id)
+    roi_arr = zarr.create(
+            shape=crop.shape,
+            chunks=full_res_arr.chunksize,
+            dtype=full_res_arr.dtype,
+            store=zarr.storage.FSStore(f"{roi_path}/0"),
+            overwrite=True,
+            dimension_separator="/"
+    )
+    z_chunk = full_res_arr.chunksize[1]
+    for z in range(0, z_end-z_start, z_chunk):
+        region = (slice(None),
+                slice(z, z+z_chunk),
+                slice(None),
+                slice(None))
+        crop[region].to_zarr(roi_arr, compute=True, region=region)
+    logger.info(f"ROI {id} saved!")
+
+    # Copy NGFF metadata from the raw image to the roi image
+    logger.info(f"Copying NGFF metadata from {zarr_path.name} to {roi_path.name}")
+    source_group = zarr.open_group(zarr_path, mode="r")
+    source_attrs = source_group.attrs.asdict()
+    logger.info(f"Saving cropping metadata to {roi_path.name}")
+    roi_group = zarr.open_group(roi_path, mode="a")
+    roi_group.attrs.put(source_attrs)
+    roi_group.attrs["crop_info"] = {
+        "roi_id": id,
+        "crop_coordinates": {
+            "z_start_um": coords['z_start_um'],
+            "z_end_um": coords['z_end_um'],
+            "y_start_um": coords['y_start_um'],
+            "y_end_um": coords['y_end_um'],
+            "x_start_um": coords['x_start_um'],
+            "x_end_um": coords['x_end_um']
+        },
+        "origin": f"{zarr_path.name}"
+    }
+    multiscales = roi_group.attrs["multiscales"]
+    multiscales[0]["name"] = id
+    roi_datasets = [multiscales[0]["datasets"][i] \
+                    for i in range(num_levels)]
+    multiscales[0]["datasets"] = roi_datasets
+    roi_group.attrs["multiscales"] = multiscales
+
+# Update FOV ROI table in case of crop
+    if crop_or_roi == "crop":
+        coords = dict(x_start=x_start, x_end=x_end, 
+                    y_start=y_start, y_end=y_end, 
+                    z_start=z_start, z_end=z_end)
+        fov_roi_table = adapt_roi_table(zarr_path, roi_path, coords, scale)
+        
+        # Write table
+        logger.info(f"Writing FOV ROI table for {roi_path.name}")
+        fov_roi_table = prepare_FOV_ROI_table(fov_roi_table)
+        write_table(
+            roi_group,
+            "FOV_ROI_table",
+            fov_roi_table,
+            overwrite=True,
+            table_attrs={"type": "roi_table"},
+        )
+
+    # Write well ROI table
+    logger.info(f"Writing well ROI table for {roi_path.name}")
+    well_table = get_single_image_ROI(roi_arr.shape[1:], scale) 
+    write_table(
+        roi_group,
+        "well_ROI_table",
+        well_table,
+        overwrite=True,
+        table_attrs={"type": "roi_table"},
+    )
+
+
+
+@validate_call
+def crop_regions_of_interest(
+    *,
+    zarr_url: str,
+    crop_or_roi: str = "roi",
+    roi_table_name: str = "roi_coords",
+    num_levels: Optional[int] = None,
+    coarsening_xy: Optional[int] = None,
+    chunksize: Optional[tuple[int, int, int]] = None,
+) -> Dict[str, Any]:
+    """
+    Crop regions of interest from a multi-channel OME-Zarr image. It loads the full
+    resolution image, crops the ROI, and saves it in the same well.
+    
+    Parameters:
+        zarr_url: Path to the individual OME-Zarr image to be processed.
+            (standard argument for Fractal tasks, managed by Fractal server).
+        crop_or_roi: Whether the coordinates are for a crop or a ROI. If `crop`, 
+            the coordinates correspond to a crop of the full resolution image to remove
+            empty space for example. A cropped_image will be output with adapted
+            FOV_ROI_table. If `roi`, one or more small ROIs are to be extracted.
+            Default: `roi`.
+        roi_table_name: Name/identifier of the ROI coordinates table to identify 
+            it in the `zarr_dir`. If not provided, the default `roi_coords` is used.
+        num_levels: Number of pyramid levels to generate for the ROI image (including 
+            the full resolution image). If not provided, the same multi-resolution
+            pyramid size as the original image will be used. Default: None.
+        coarsening_xy: Coarsening factor in XY for the ROI image. Optional, if different
+            from the coarsening factor used for the full resolution image. Default: 2.
+        chunksize: Chunk size to use for the new ROI image(s). If None, the chunksize
+            of the original image will be used. Default: None.
+    """
+    zarr_path = Path(zarr_url)
+    logger.info(f"Start task: `Crop Region of Interest` "
+                f"for {zarr_path.parent.name}/{zarr_path.name}")
+    
+    cluster = _set_dask_cluster()
+
+    # Load full resolution image and NGFF metadata
+    logger.info(f"Loading full resolution image...")
+    full_res_arr = da.from_zarr(zarr_path/"0")
+    image_meta = load_NgffImageMeta(str(zarr_path))
+    scale = image_meta.get_pixel_sizes_zyx(level=0)
+    if chunksize is None:
+        chunksize = full_res_arr.chunksize
+    if num_levels is None:
+        num_levels = image_meta.num_levels
+    if coarsening_xy is None:
+        coarsening_xy = image_meta.coarsening_xy
+        if coarsening_xy is None:
+            coarsening_xy = 2
+
+    # Read ROI coordinates
+    logger.info("Loading ROI coordinates table.")
+    if roi_table_name is None:
+        roi_table_name = "roi_coords"
+    tables = []
+    for path in Path(zarr_path.parent).glob(f"{roi_table_name}.csv"):
+        tables.append(path)
+    if len(tables) != 1:
+        logger.error(f"Unique ROI coordinates table not found in {zarr_path.parent}.")
+        raise FileNotFoundError
+    roi_table = pd.read_csv(tables[0], index_col=0)
+
+    # Prepare parallelisation list
+    nb_rois = len(roi_table)
+    logger.info(f"Preparing parallelisation list for {nb_rois} ROIs.")
+    parallelisation_list = []
+    image_list_updates = []
+    for roi_id, roi_row in roi_table.iterrows():
+        if crop_or_roi == "crop":
+            if len(roi_table) != 1:
+                logger.error("Number of ROIs in table and crop_or_roi parameters are "
+                             "inconsistent. Table cannot have more than one ROI if "
+                             "crop_or_roi is set to 'crop'.")
+                raise ValueError
+            logger.info(f"Task set to produce a crop from original image (e.g. to reduce size).")
+            roi_id = zarr_path.name + "_cropped"
+            roi_type = "is_crop"
+        else:
+            roi_type = "is_roi"
+        roi_path = Path(zarr_path.parent, str(roi_id))
+        parallelisation_list.append(
+            dict(
+                roi_id=roi_id,
+                roi_coords=roi_row.to_dict(),
+                roi_path=roi_path
+            )
+        )
+        image_list_updates.append(dict(zarr_url=str(roi_path), 
+                                       origin=str(zarr_path), 
+                                       attributes=dict(image=roi_id),
+                                       types={roi_type: True}))
+    with Client(cluster) as client:
+        if crop_or_roi == "crop":
+            crop_id = parallelisation_list[0]["roi_id"]
+            crop_coords = parallelisation_list[0]["roi_coords"]
+            crop_path = parallelisation_list[0]["roi_path"]
+            save_roi_parallel(zarr_path, 
+                              full_res_arr=full_res_arr,
+                              id=crop_id, 
+                              coords=crop_coords, 
+                              scale=tuple(scale),
+                              num_levels=num_levels,
+                              coarsening_xy=coarsening_xy,
+                              crop_or_roi=crop_or_roi)
+            
+            # Write pyramid of resolution
+            logger.info(f"Building pyramid of resolution for {crop_path.name}")
+            build_pyramid(
+                zarrurl=crop_path,
+                overwrite=True,
+                num_levels=num_levels,
+                coarsening_xy=coarsening_xy,
+                chunksize=chunksize,
+            )
+            
+            # Re-compute optimal contrast limits for ROI
+            contrast_limits = _determine_optimal_contrast(crop_path, num_levels, segment_sample=True)
+            _update_omero_channels(crop_path, {"window": contrast_limits})
+        else:
+            futures = []
+            for i, roi_params in enumerate(parallelisation_list):
+                fut = client.submit(save_roi_parallel, 
+                            zarr_path=zarr_path,
+                            full_res_arr=full_res_arr,
+                            id=roi_params["roi_id"],
+                            coords=roi_params["roi_coords"],
+                            scale=scale,
+                            num_levels=num_levels,
+                            coarsening_xy=coarsening_xy,
+                            pure=False,
+                            retries=1)
+                futures.append(fut)
+            client.gather(futures)
+            for i, roi_params in enumerate(parallelisation_list):
+                fut = client.submit(build_pyramid, 
+                            zarrurl=roi_params["roi_path"],
+                            overwrite=True,
+                            num_levels=num_levels,
+                            coarsening_xy=coarsening_xy,
+                            chunksize=chunksize,
+                            pure=False,
+                            retries=1)
+                futures.append(fut)
+            client.gather(futures)
+
+            # Re-compute optimal contrast limits for ROI
+            for i, roi_params in enumerate(parallelisation_list):
+                contrast_limits = _determine_optimal_contrast(
+                    roi_params["roi_path"], 
+                    num_levels, 
+                    segment_sample=True)
+                _update_omero_channels(roi_params["roi_path"], {"window": contrast_limits})
+    
+    image_list_updates = dict(
+        image_list_updates=image_list_updates
+    )
+    return image_list_updates
+
+if __name__ == "__main__":
+    from fractal_task_tools.task_wrapper import run_fractal_task
+
+    run_fractal_task(
+        task_function=crop_regions_of_interest,
+        logger_name=logger.name,
+    )
